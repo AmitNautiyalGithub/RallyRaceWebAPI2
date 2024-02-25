510dda86264aaa88a69b83903be2c5ecce519c34
AmitNautiyal <59432251+AmitNautiyalGithub@users.noreply.github.com>
2024-02-25 15:42:02 +0530

    RRW-1:Feature: Create Car

---
+++ RallyRaidWebApi/WebUi/Features/Cars/CarsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace WebUi.Features.Cars
+{
+    [Route("api/cars")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        //static List<Car> cars = new List<Car>()
+        //{
+        //    new Car(){Id= 1 , TeamName="Team A", Speed = 100, MelfunctionChance = 0.2},
+        //    new Car(){Id= 2 , TeamName="Team B", Speed = 90, MelfunctionChance = 0.1},
+        //    new Car(){Id= 3 , TeamName="Team C", Speed = 80, MelfunctionChance = 0.15},
+        //};
+
+        public ActionResult<List<Car>> GetCars()
+        {
+            List<Car> cars = new List<Car>()
+            {
+                new Car(){Id= 1 , TeamName="Team A", Speed = 100, MelfunctionChance = 0.2},
+                new Car(){Id= 2 , TeamName="Team B", Speed = 90, MelfunctionChance = 0.1},
+                new Car(){Id= 3 , TeamName="Team C", Speed = 80, MelfunctionChance = 0.15},
+            };
+            return Ok(cars);
+        }
+    }
+}
